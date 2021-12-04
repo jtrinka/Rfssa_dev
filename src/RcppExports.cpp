d8a64f33dd33b2b35233e9c18c3e730b766a11ac
Jordan Trinka <jordantrinka4@hotmail.com>
2021-12-03 17:26:55 -0800

    Update decomposition to be faster.

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -2,6 +2,7 @@
 // Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 #include <RcppArmadillo.h>
+#include <RcppEigen.h>
 #include <Rcpp.h>
 
 using namespace Rcpp;
@@ -54,6 +55,40 @@ BEGIN_RCPP
     Rcpp::traits::input_parameter< int >::type L(LSEXP);
     Rcpp::traits::input_parameter< NumericVector >::type cx(cxSEXP);
     rcpp_result_gen = Rcpp::wrap(Cofmat(d, L, cx));
+    return rcpp_result_gen;
+END_RCPP
+}
+// CalculateInverse
+SEXP CalculateInverse(const Eigen::Map<Eigen::MatrixXd> A);
+RcppExport SEXP _Rfssa_CalculateInverse(SEXP ASEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< const Eigen::Map<Eigen::MatrixXd> >::type A(ASEXP);
+    rcpp_result_gen = Rcpp::wrap(CalculateInverse(A));
+    return rcpp_result_gen;
+END_RCPP
+}
+// AtimesB
+SEXP AtimesB(const Eigen::Map<Eigen::MatrixXd> A, Eigen::Map<Eigen::MatrixXd> B);
+RcppExport SEXP _Rfssa_AtimesB(SEXP ASEXP, SEXP BSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< const Eigen::Map<Eigen::MatrixXd> >::type A(ASEXP);
+    Rcpp::traits::input_parameter< Eigen::Map<Eigen::MatrixXd> >::type B(BSEXP);
+    rcpp_result_gen = Rcpp::wrap(AtimesB(A, B));
+    return rcpp_result_gen;
+END_RCPP
+}
+// EigenDecomp
+Rcpp::List EigenDecomp(const Eigen::Map<Eigen::MatrixXd> A);
+RcppExport SEXP _Rfssa_EigenDecomp(SEXP ASEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< const Eigen::Map<Eigen::MatrixXd> >::type A(ASEXP);
+    rcpp_result_gen = Rcpp::wrap(EigenDecomp(A));
     return rcpp_result_gen;
 END_RCPP
 }
@@ -153,6 +188,9 @@ static const R_CallMethodDef CallEntries[] = {
     {"_Rfssa_HLinprod", (DL_FUNC) &_Rfssa_HLinprod, 3},
     {"_Rfssa_HpLinprod", (DL_FUNC) &_Rfssa_HpLinprod, 4},
     {"_Rfssa_Cofmat", (DL_FUNC) &_Rfssa_Cofmat, 3},
+    {"_Rfssa_CalculateInverse", (DL_FUNC) &_Rfssa_CalculateInverse, 1},
+    {"_Rfssa_AtimesB", (DL_FUNC) &_Rfssa_AtimesB, 2},
+    {"_Rfssa_EigenDecomp", (DL_FUNC) &_Rfssa_EigenDecomp, 1},
     {"_Rfssa_SS", (DL_FUNC) &_Rfssa_SS, 4},
     {"_Rfssa_Gram", (DL_FUNC) &_Rfssa_Gram, 4},
     {"_Rfssa_SSM", (DL_FUNC) &_Rfssa_SSM, 6},
--- src/Utils.cpp
+++ src/Utils.cpp
@@ -1,7 +1,9 @@
-#include <Rcpp.h>
+#include <RcppEigen.h>
 #include <list>
 
 using namespace Rcpp;
+
+// [[Rcpp::depends(RcppEigen)]]
 
 // Module  function
 //' @importFrom Rcpp sourceCpp
@@ -30,6 +32,39 @@ NumericMatrix Cofmat (int d,int L, NumericVector cx)
     }
   }
   return S;
+}
+
+// Matrix Inversion
+//
+//'@importFrom Rcpp sourceCpp
+// [[Rcpp::export]]
+SEXP CalculateInverse(const Eigen::Map<Eigen::MatrixXd> A)
+{
+
+  Eigen::PartialPivLU<Eigen::MatrixXd> lu(A);
+
+  return Rcpp::wrap(lu.inverse());
+
+}
+
+//'@importFrom Rcpp sourceCpp
+// [[Rcpp::export]]
+SEXP AtimesB(const Eigen::Map<Eigen::MatrixXd> A, Eigen::Map<Eigen::MatrixXd> B){
+  Eigen::MatrixXd C = A * B;
+
+  return Rcpp::wrap(C);
+}
+
+// Eigendecomposition
+//
+//'@importFrom Rcpp sourceCpp
+// [[Rcpp::export]]
+Rcpp::List EigenDecomp(const Eigen::Map<Eigen::MatrixXd> A)
+{
+
+  Eigen::EigenSolver<Eigen::MatrixXd> es(A);
+  return Rcpp::List::create(Rcpp::Named("values")=Rcpp::wrap(es.eigenvalues()), Rcpp::Named("vectors")=Rcpp::wrap(es.eigenvectors()));
+
 }
 
 
